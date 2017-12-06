17e0467b661c55c61f0b1403d37485548406624b
window sun <sun8wd@gmail.com>
2017-12-06 08:59:08 +0800

    IU-2017.3 <sun8wd@sunwendongdeMacBook-Pro.local Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, ide.general.xml, project.default.xml, diff.xml, git.xml, other.xml, Default.xml, file.template.settings.xml, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, gradle.run.settings.xml, colors.scheme.xml, path.macros.xml, databaseDrivers.xml, mavenVersion.xml, Ladies Night 2.icls, Obsidian.icls, Default.xml, Solarized Light _Alternate_.icls, parameter.hints.xml, Class.java, jdk.table.xml, laf.xml, Eclipse _Mac OS X_ copy.xml, web-browsers.xml, proxy.settings.xml, databaseSettings.xml, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+* ${DESCRIPTION}
+* 
+* @author <a href="mailto:sunwendong@celloud.cn">sunwd</a>
+* @create ${DATE} ${TIME}
+*/
+public class ${NAME} {
+}
